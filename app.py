74ed9dcee6ae4fa487a894170746bd9610c853e8
Sandeep Kasturi <112337452+Sandeepkasturi@users.noreply.github.com>
2023-12-16 10:23:33 +0530

    Code

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
-# Sandeep Kasturi
-# Code from Google Colab- Goolge AI Studio for Developers
-import pathlib
 import textwrap
-import google.generativeai as genai
-from IPython.display import display
-from IPython.display import Markdown
+from IPython.display import display, Markdown
+from google.generativeai import genai
 
-
+# Function to convert plain text to Markdown format
 def to_markdown(text):
-  text = text.replace('•', '  *')
-  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+    text = text.replace('•', '  *')
+    return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
-# Or use `os.getenv('GOOGLE_API_KEY')` to fetch an environment variable.
-#GOOGLE_API_KEY=userdata.get('GOOGLE_API_KEY')
-
+# Set up the Generative AI configuration with the API key
 genai.configure(api_key='YOUR_OWN_API_KEY')
+
+# Create a Generative Model instance (assuming 'gemini-pro' is a valid model)
 model = genai.GenerativeModel('gemini-pro')
-response = model.generate_content("What is the meaning of life?") #Here you can write your own query.
-print(to_markdown(response.text))
-print(response.prompt_feedback)
-print(response.candidates)
+
+# Get user input for the query
+user_query = input("Ask the model a question: ")
+
+# Generate content based on the user's query
+response = model.generate_content(user_query)
+
+# Display the generated content in Markdown format
+display(to_markdown(response.text))
+
+# Display additional information for analysis
+print(f"Prompt Feedback: {response.prompt_feedback}")
+print(f"Candidates: {response.candidates}")
