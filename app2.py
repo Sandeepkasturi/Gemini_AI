e59389a172877c558dbadb6819d808cad359b0dd
Sandeep Kasturi <112337452+Sandeepkasturi@users.noreply.github.com>
2023-12-29 16:43:40 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,21 +1,37 @@
-import pathlib
 import textwrap
+from IPython.display import display, Markdown
 import google.generativeai as genai
-from IPython.display import display
-from IPython.display import Markdown
 
 
+# Function to convert plain text to Markdown format
 def to_markdown(text):
-  text = text.replace('•', '  *')
-  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+    text = text.replace('•', '  *')
+    return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
-# Or use `os.getenv('GOOGLE_API_KEY')` to fetch an environment variable.
-#GOOGLE_API_KEY=userdata.get('GOOGLE_API_KEY')
 
-genai.configure(api_key='YOUR_OWN_API_KEY')
+# Set up the Generative AI configuration with the API key
+genai.configure(api_key='AIzaSyBOc7WOykXVHvnU-GsMgCYZwoBqFERjQFI')
+
+# Create a Generative Model instance (assuming 'gemini-pro' is a valid model)
 model = genai.GenerativeModel('gemini-pro')
-query = input("Ask the model a Question: ")
-response = model.generate_content(query)
-print(to_markdown(response.text))
-print(response.prompt_feedback)
-print(response.candidates)
+
+while True:
+    # Get user input for the query
+    user_query = input("Ask the model a question (or enter 'exit' to quit): ")
+
+    if user_query.lower() == 'exit':
+        break
+
+    # Generate content based on the user's query
+    response = model.generate_content(user_query)
+
+    # Display the generated content in Markdown format
+    display(to_markdown(response.text))
+
+    # Display additional information for analysis
+    print(f"Prompt Feedback: {response.prompt_feedback}")
+    print(f"Candidates: {response.candidates}")
+
+    for chunk in response:
+        print(chunk.text)
+        print("_" * 80)
